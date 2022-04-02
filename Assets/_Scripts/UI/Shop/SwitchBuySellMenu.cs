4a2352cc8920b5316fdc2d16664f79e3e43b63f2
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2022-04-01 20:12:50 -0400

    add shop label

--- Assets/_Scripts/UI/Shop/SwitchBuySellMenu.cs
+++ Assets/_Scripts/UI/Shop/SwitchBuySellMenu.cs
@@ -7,7 +7,10 @@ using UnityEngine.UI;
 public class SwitchBuySellMenu : MonoBehaviour
 {
     [SerializeField] private GameObject buyMenu;
+    [SerializeField] private TextMeshProUGUI stateText;
+
     [SerializeField] private GameObject sellMenu;
+
     [SerializeField] public TextMeshProUGUI btnText;
 
     // Start is called before the first frame update
@@ -16,18 +19,22 @@ public class SwitchBuySellMenu : MonoBehaviour
         GetComponent<Button>().onClick.AddListener(SwapWindows);
 
         //set default values
-        sellMenu.SetActive(false);
         buyMenu.SetActive(true);
+        sellMenu.SetActive(false);
+
         btnText.text = "SellMenu";
+        stateText.text = "Current Menu: BuyMenu";
 
     }
 
     void SwapWindows()
     {
         buyMenu.SetActive(!buyMenu.activeSelf);
-        sellMenu.SetActive(!buyMenu.activeSelf);//makes sure that sell menu is opposite of buy menu (prevent it from going into a state where both display at the same time)
-        btnText.text = buyMenu.activeSelf ? "SellMenu" : "BuyMenu";
+        sellMenu.SetActive(!sellMenu.activeSelf);//makes sure that sell menu is opposite of buy menu (prevent it from going into a state where both display at the same time)
 
+        btnText.text = buyMenu.activeSelf ? "SellMenu" : "BuyMenu";
+        stateText.text = "Current Menu: " + (!buyMenu.activeSelf ? "SellMenu" : "BuyMenu");
     }
 
+    
 }
