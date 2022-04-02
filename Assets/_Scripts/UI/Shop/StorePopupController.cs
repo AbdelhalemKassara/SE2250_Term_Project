73a3d149fab0f275cc8fcb6934adecb775c993d2
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2022-04-01 23:09:41 -0400

    modified the storePopUpController so it won"t disable itself

--- Assets/_Scripts/UI/Shop/StorePopupController.cs
+++ Assets/_Scripts/UI/Shop/StorePopupController.cs
@@ -13,24 +13,34 @@ public class StorePopupController : MonoBehaviour
     private bool setupStore = true;
     private bool unSetupStore = false;
 
+    private GameObject panel;
+
+    private void Start()
+    {
+        panel = transform.Find("Panel").gameObject;
+        panel.SetActive(false);
+    }
+
     void Update()
     {
        if((player.transform.position - storeManagerNPC.transform.position).sqrMagnitude < triggerRadSqr)
         {
             if(setupStore)
             {
-                gameObject.SetActive(true);
+                panel.SetActive(true);
                 sellStore.AddItemsFromPlayerInventory();
 
                 setupStore = false;
                 unSetupStore = true;
+
             }
         } else
         {
             if(unSetupStore)
             {
-                gameObject.SetActive(false);
+
                 sellStore.ClearPlayerItems();
+                panel.SetActive(false);
 
                 unSetupStore = false;
                 setupStore = true;
